873d5b6f92bf948cf79b94ae4c676fec08144fda
harishpalani2023 <harish.palani@cloudrizon.com>
2023-11-25 21:27:24 +0100

    CNN with spectrogram

--- AudioClassification.py
+++ AudioClassification.py
@@ -12,7 +12,22 @@ from scipy.stats import kurtosis,skew,mode
 from sklearn.preprocessing import StandardScaler
 from sklearn import svm
 from sklearn.metrics import accuracy_score,confusion_matrix
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn import svm
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import StratifiedShuffleSplit,StratifiedKFold,train_test_split
+from keras import utils
+import keras
+from keras import layers
+from keras.layers import Activation, Dense, Dropout, Conv1D, Conv2D, Flatten,Reshape, BatchNormalization, ZeroPadding2D,MaxPooling1D,AveragePooling1D, MaxPooling2D, GlobalMaxPooling2D, GlobalAveragePooling1D, AveragePooling2D, Input, Add
+from keras.models import Sequential
+from keras import regularizers,optimizers
+from keras.optimizers import SGD,Adam
+from keras.utils import to_categorical
+import keras.backend as K
+from keras.models import load_model
+from keras.callbacks import EarlyStopping
+import warnings
+warnings.filterwarnings("ignore")
 
 # Function to extract the training data and convert to numpy files
 def get_traindata(dir_path,path_traindata):
@@ -92,10 +107,10 @@ def get_mfcc_features(path_to_train, csv_file, extracted_folder):
   return mfcc_features
 
 # Function to retrive all the labels of training data
-def get_labels(original_path,csv_file):
+def get_labels(path_to_dir,csv_file):
 
   # Load the csv file into a dataframe.
-  df = pd.read_csv(os.path.join(original_path, csv_file ))
+  df = pd.read_csv(os.path.join(path_to_dir, csv_file ))
   # Return the labels.
   labels = np.array(df['Class ID'])
 
@@ -159,15 +174,130 @@ def svm_classifier(X_train,Y_train,X_test):
   
   return Y_test
 
+# Function to generate spectrograms
+"""def generate_spectrograms(dir_path, csv_file, extracted_folder):
+    df = pd.read_csv(os.path.join(dir_path, csv_file))
+    audio_extracted = np.array(df['file name'])
+
+    for i in range(len(audio_extracted)):
+        audio_file_path = os.path.join(dir_path, extracted_folder, str(audio_extracted[i].replace(".wav", ".npy")))
+        audio_file_data = np.load(audio_file_path)
+        spectrogram = librosa.feature.melspectrogram(y=audio_file_data, sr=44100)
+        spectrogram_db = librosa.power_to_db(spectrogram, ref=np.max)
+         plt.figure(figsize=(10, 4))
+        librosa.display.specshow(spectrogram_db, x_axis='time', y_axis='mel', sr=44100, fmax=8000)
+        plt.colorbar(format='%+2.0f dB')
+        plt.title(f'Spectrogram for {audio_extracted[i]}')
+        plt.savefig(os.path.join(dir_path, 'spectrograms', f'spectrogram_{audio_extracted[i].replace(".wav", ".png")}'))
+        plt.close() """
+
+def get_spectrograms(path_to_dir, train_csv , train_extracted, test_csv ,test_extracted, target_shape=(128, 128)):
+
+  # Read the train_csv for Training Dataset file names.
+  df = pd.read_csv(os.path.join(path_to_dir, train_csv ))
+  audio_extracted = np.array(df['file name'])
+  labels = np.array(df['Class ID'])
+
+  # Intializing empty list for storing spectrograms and labels.
+  X_train = []
+  Y_train = []
+  X_test = []  
+
+  # Function to resize or pad the spectrogram to the target shape
+  def resize_spectrogram(spec, target_shape):
+      if spec.shape != target_shape:
+          spec = np.pad(spec, ((0, max(0, target_shape[0] - spec.shape[0])), (0, max(0, target_shape[1] - spec.shape[1]))), mode='constant')
+      return spec
+
+  # Looping through the Training Data Audio sequences.
+  for i in range(len(audio_extracted)):
+        # Load Audio Sequence, calculate the Mel spectrogram and append it to list.
+        audio_file_data = np.load(os.path.join(path_to_dir, train_extracted, str(audio_extracted[i].replace(".wav", ".npy"))))
+        mel_spec = librosa.power_to_db(librosa.feature.melspectrogram(y=audio_file_data[:650000], n_mels=target_shape[0], hop_length=2048), ref=np.max)
+        mel_spec_resized = resize_spectrogram(mel_spec, target_shape)
+        X_train.append(mel_spec_resized)
+        Y_train.append(labels[i])  
+
+  # Read the test_csv for Testing Dataset file names.
+  df = pd.read_csv(os.path.join(path_to_dir, test_csv))
+  audio_extracted = np.array(df['file name'])
+
+  # Looping through the testing data audio sequences.
+  for i in range(len(audio_extracted)):
+        # Load Audio Sequence, calculate the Mel spectrogram and append it to list.
+        audio_file_data = np.load(os.path.join(path_to_dir, test_extracted, str(audio_extracted[i].replace(".wav", ".npy"))))
+        mel_spec = librosa.power_to_db(librosa.feature.melspectrogram(y=audio_file_data[:650000], n_mels=target_shape[0], hop_length=2048), ref=np.max)
+        mel_spec_resized = resize_spectrogram(mel_spec, target_shape)
+        X_test.append(mel_spec_resized)
+  
+  
+  # Converting the lists into numpy arrays.
+  X_train = np.array(X_train)
+  Y_train = np.array(Y_train).reshape(len(Y_train),1)
+  X_test = np.array(X_test)
+
+  # Splitting the training features into Training and Validation.
+  stratified_split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+  for train_index, test_index in stratified_split.split(X_train, Y_train):
+        training_data,train_labels=X_train[train_index],Y_train[train_index]
+        val_data,val_labels=X_train[test_index],Y_train[test_index] 
+  
+  # One hot encoding the Training and validation labels.
+  Y_train = to_categorical(train_labels,6)
+  Y_val = to_categorical(val_labels,6)
+
+# Reshaping the features into the form (rows, columns, 1) for Convolutional Neural Networks.
+  X_train = [training_data[i].reshape(target_shape[0], target_shape[1], 1) for i in range(len(training_data))]
+  X_val = [val_data[i].reshape(target_shape[0], target_shape[1], 1) for i in range(len(val_data))]
+  X_test = [X_test[i].reshape(target_shape[0], target_shape[1], 1) for i in range(len(X_test))]
+
+  return  X_train,Y_train,X_val,Y_val,X_test
+
+#Function to use CNN classifier
+def cnn_classifier(inputShape):
+
+    # Intializing the model sequential.
+    model = Sequential()
+
+    # Adding Layers.  
+    model.add(Conv2D(32, (4, 4), input_shape=inputShape))
+    model.add(MaxPooling2D((4, 4)))
+    model.add(Activation('relu'))
+    model.add(BatchNormalization())
+
+    """ model.add(Conv2D(64, (4, 4), padding="same"))
+    model.add(MaxPooling2D((4, 4)))
+    model.add(Activation('relu'))
+    model.add(BatchNormalization()) """
+
+    model.add(Conv2D(64, (2, 2), padding="same"))
+    model.add(MaxPooling2D((2, 2)))
+    model.add(Activation('relu'))
+    model.add(BatchNormalization())
+
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(6,activation='softmax'))
+    
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])  
+
+    print(model.summary())
+    
+    return model
+        
+       
+
 #-------------MAIN-------------# 
-path_to_dir = 'C:/Users/Gurunag Sai/OneDrive/Desktop/project/AudioClassification'
+path_to_dir = 'E:/Individual Project/AudioClassification-main/AudioClassification-main'
 path_traindata = 'DataSet/Training Dataset'
 path_testdata = 'DataSet/Test Dataset'
 
 # Run the below two lines of code to extract the datset and convert them to Numpy files
-get_traindata(path_to_dir,path_traindata)
-get_testdata(path_to_dir,path_testdata)
- 
+#get_traindata(path_to_dir,path_traindata)
+#get_testdata(path_to_dir,path_testdata)
+
+'''
 #-------------Support Vector Machine with MFCC model-------------# 
 # Get the training and testing data preprocessed with MFCC
 X_train = get_mfcc_features(path_to_dir,'TrainDataSetCSV.csv','train_extracted')
@@ -187,3 +317,24 @@ df = pd.read_csv(os.path.join(path_to_dir,'SVM_MFCC_predict.csv'))
 Y_true = df['Actual Class ID']
 Y_test = df['Predict Class ID']
 plot_confusion_matrix(Y_true,Y_test,"SVM_MFCC")
+'''
+#------------- CNN with Spectrogram-------------# 
+# Get the spectrograms
+X_train,Y_train,X_val,Y_val, X_test = get_spectrograms(path_to_dir,'TrainDataSetCSV.csv','train_extracted', 'TestDataSetCSV.csv','test_extracted', target_shape=(128, 128))
+
+# Applying 2-D Convolution Neural Network on the spectrograms.
+model = cnn_classifier(X_train[0].shape)
+model.fit(np.array(X_train), Y_train, epochs=10, batch_size=32, validation_data= (np.array(X_val), Y_val))
+
+# Predict the Test dataset using the model and save results to a CSV file.
+Y_test_nn = np.argmax(model.predict(np.array(X_test)),axis=1)
+Y_test = pd.read_csv(os.path.join(path_to_dir,'TestDataSetCSV.csv'))
+Y_test['Predict Class ID'] = Y_test_nn.tolist()
+Y_test = Y_test.rename(columns={'new_id':'id'})
+Y_test.to_csv(os.path.join(path_to_dir,'CNN_Spectrogram.csv'),index=False)
+
+# Plot Confusion matrix using validation predictions.
+df = pd.read_csv(os.path.join(path_to_dir,'CNN_Spectrogram.csv'))
+Y_true = df['Actual Class ID']
+Y_test = df['Predict Class ID']
+plot_confusion_matrix(Y_true,Y_test,"CNN_Spectrogram")
